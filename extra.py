e88ab735d83991d0cf781e83f205e13baa085979
Michon van Dooren <michon1992@gmail.com>
2014-06-13 09:50:25 +0200

    Fixed a small bug with the page time and closingiterator objects.

--- extra.py
+++ extra.py
@@ -1,6 +1,7 @@
 import time
 
 from flask import request
+from werkzeug.wsgi import ClosingIterator
 
 def timer_start():
     """
@@ -12,10 +13,13 @@ def timer_end(response):
     """
     End the timing of the request time, and insert the time into the page result.
     """
+    if isinstance(response.response, ClosingIterator):
+        return response
+
     diff = time.time() - request.start
     del request.start
 
-    if (response.response):
+    if response.response:
         response.response[0] = response.response[0].replace('__EXECUTION_TIME__', '{:.3}'.format(diff))
         response.headers["content-length"] = len(response.response[0])
 
