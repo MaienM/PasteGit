0b6f7ebb6313e4271617ab84486c587985a27fd7
Michon van Dooren <michon1992@gmail.com>
2014-07-11 11:56:43 +0200

    The anchor-scroll-javascript now also updates the URL.

--- static/js/view.js
+++ static/js/view.js
@@ -10,6 +10,7 @@ $(function() {
 		$('html, body').animate({
 			scrollTop: $($(this).attr('href')).offset().top - 120,
 		}, 800);
+		window.location.href = window.location.href.split('#')[0] + $(this).attr('href');
 		e.preventDefault();
 	});
 });
